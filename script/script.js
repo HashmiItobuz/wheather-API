3e43cf4aad6c64c53ed596f92020feb197b4929e
Md Hashmi <md.hashmi@itobuz.com>
2023-05-31 14:35:18 -0700

    optimize code

--- script/script.js
+++ script/script.js
@@ -1,23 +1,17 @@
 let inputContainer = document.getElementById("inputBox");
 let placeName = document.getElementById("PlaceName");
-let temp = document.getElementById("temperature");
+let locationTemp = document.getElementById("temperature");
 let feelsLike = document.getElementById("feelsLike");
 let wheatherImg = document.getElementById("wheatherImage");
-let feelsLikeContain= document.querySelector(".weather-content");
-let locationTemp = document.querySelector(".loc-temp");
 let wheatherDescription = document.querySelector(".wheather-description");
-let degree = document.getElementById("degree");
 
-wheatherImg.classList.add("hidden");
-locationTemp.classList.add("hidden");
-feelsLikeContain.classList.add("hidden");
 wheatherDescription.classList.add("hidden");
 
 var currWidth = wheatherImg.clientWidth;
-inputContainer.addEventListener("keyup", (e)=>{
-    if(e.key === "Enter"){
-        getData();
-    }
+inputContainer.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    getData();
+  }
 });
 
 async function getData() {
@@ -25,45 +19,33 @@ async function getData() {
     const res = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${inputContainer.value}&aqi=no`
     ).then((res) => res.json());
-    
-    placeName.innerHTML=res.location.name;
-    temp.innerHTML=res.current.temp_c;
-    feelsLike.innerHTML=res.current.feelslike_f;
-    locationTemp.classList.remove("font-size");
-    degree.classList.remove("hidden");
 
-    if(res.current.temp_c >= 40){
-        wheatherImg.src="images/sunny.png";
-        unhidden();
+    placeName.innerHTML = res.location.name;
+    locationTemp.innerHTML = res.current.temp_c + "<sup>o</sup>";
+    feelsLike.innerHTML = "Feels " + res.current.feelslike_f + "<sup>o</sup>";
+    wheatherImg.classList.add("img");
+    wheatherDescription.classList.remove("hidden");
+    locationTemp.classList.remove("error-font-size");
+    wheatherImg.classList.remove("error-img");
+
+    if (res.current.temp_c >= 40) {
+      wheatherImg.src = "images/sunny.png";
     }
-    else if(res.current.temp_c  > 20){
-        wheatherImg.src="images/sun_behind.png";
-        unhidden();
+    else if (res.current.temp_c > 20) {
+      wheatherImg.src = "images/sun_behind.png";
     }
     else {
-        wheatherImg.src="images/rain.png";
-        unhidden();
+      wheatherImg.src = "images/rain.png";
     }
-    inputContainer.value="";
+    inputContainer.value = "";
   } catch {
-    wheatherImg.classList.remove("hidden");
-    locationTemp.classList.remove("hidden");
-    degree.classList.add("hidden");
-    placeName.classList.add("hidden");
-    wheatherImg.src="images/cloud.png";
-    wheatherImg.style.width = (currWidth + 200) + "px";
-    temp.innerHTML="No Data Found";
-    locationTemp.classList.add("font-size");
-    feelsLike.innerHTML="";
-    feelsLikeContain.classList.add("hidden");
+    placeName.innerHTML = "";
+    wheatherImg.src = "images/cloud.png";
+    locationTemp.innerHTML = "No Data Found";
+    feelsLike.innerHTML = "";
     wheatherDescription.classList.add("hidden");
-    inputContainer.value="";
+    inputContainer.value = "";
+    locationTemp.classList.add("error-font-size");
+    wheatherImg.classList.add("error-img");
   }
-}
-function unhidden() {
-    wheatherImg.classList.remove("hidden");
-    placeName.classList.remove("hidden");
-    locationTemp.classList.remove("hidden");
-    feelsLikeContain.classList.remove("hidden");
-    wheatherDescription.classList.remove("hidden");
 }
